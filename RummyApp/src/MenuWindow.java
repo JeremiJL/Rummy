1802b4effe47a1dd8c513bd1ff70531a4388ef04
JeremiJL <j3remilipniacki@gmail.com>
2024-02-03 23:40:55 +0100

    interactive board, can 'drag and drop' functionality

---
+++ RummyApp/src/MenuWindow.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuWindow extends AbstractWindow {
+
+    //Components
+    private JPanel panelMain;
+    private JLabel labelTitle;
+    private JButton buttonHost;
+    private JButton buttonJoin;
+    private JButton buttonOptions;
+    private JButton buttonProfile;
+
+    public MenuWindow() {
+        //Invoking abstract window init
+        super();
+        //Setting contentPanel
+        this.setContentPane(panelMain);
+        //Creating action listeners
+        initListeners();
+    }
+
+
+    @Override
+    protected void initListeners() {
+
+        //Button Join - sends you to gameplay screen
+        buttonJoin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new GameWindow();
+            }
+        });
+
+        //Button Host - sends you to gameplay screen
+        buttonHost.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new GameWindow();
+            }
+        });
+
+        //Button Options - sends you to options screen
+        buttonOptions.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new OptionsWindow();
+            }
+        });
+
+    }
+
+}
